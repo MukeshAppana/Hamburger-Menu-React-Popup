78645127dc416411201f7843284bc960c0eba437
MukeshAppana <109844172+MukeshAppana@users.noreply.github.com>
2023-01-30 12:43:11 +0530

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,22 @@
+import Header from '../Header'
+
 import './index.css'
 
 const About = () => (
   <div className="about-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
-      alt="about"
-      className="about-image"
-    />
+    <Header />
+    <div className="about-image-container">
+      <img
+        className="mobile-about-image"
+        src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+        alt="About"
+      />
+      <img
+        className="desktop-about-image"
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+        alt="About"
+      />
+    </div>
   </div>
 )
 
