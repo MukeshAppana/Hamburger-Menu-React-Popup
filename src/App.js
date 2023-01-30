bf32ce17b8877925d194d963f1629b5c8bed51dd
MukeshAppana <109844172+MukeshAppana@users.noreply.github.com>
2023-01-30 12:46:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import {Route, Switch, Redirect} from 'react-router-dom'
 
-import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import NotFound from './components/NotFound'
 
+import './App.css'
+
 const App = () => (
-  <div>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/not-found" component={NotFound} />
-      <Redirect to="not-found" />
-    </Switch>
-  </div>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
 )
 
 export default App
